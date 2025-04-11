f32d0b5d3cc7d8e479b31d124ac1d4ce8e0de140
AlanSarasate <alansarasatecc@gmail.com>
2025-04-11 19:57:02 -0300

    feat: Adicionando estrutura inicial do Redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import {Routes} from './routes'
 import { BrowserRouter } from 'react-router-dom'
 import { CartProvider } from './hooks/useCart'
 import './global.css'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-      <CartProvider>
-        <Routes/>
-      </CartProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <CartProvider>
+          <Routes/>
+        </CartProvider>
+      </BrowserRouter>
+    </Provider>
     </>
   )
 }
